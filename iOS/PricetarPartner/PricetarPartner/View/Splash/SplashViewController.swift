e4e634c1b137e02b2ed5d8c1fa68e5b60470731e
Leapfrog-Inc <developer@lfg.co.jp>
2021-02-21 14:35:29 +0900

    [iOS] スプラッシュ画面を作成

--- iOS/PricetarPartner/PricetarPartner/View/Splash/SplashViewController.swift
+++ iOS/PricetarPartner/PricetarPartner/View/Splash/SplashViewController.swift
@@ -8,9 +8,57 @@
 import UIKit
 
 class SplashViewController: UIViewController {
+    
+    @IBOutlet private weak var logoVerticalConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var loginView: UIView!
+    @IBOutlet private weak var emailTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var registerView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.logoVerticalConstraint.constant = 0
+        self.loginView.isHidden = true
+        self.loginView.alpha = 0
+        self.registerView.isHidden = true
+        self.registerView.alpha = 0
 
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+            self.fetch()
+        })
+    }
+    
+    private func fetch() {
+        self.showLoginView()
+    }
+    
+    private func showLoginView() {
+        
+        self.logoVerticalConstraint.constant = -220
+        UIView.animate(withDuration: 0.6, delay: 0, options: .curveEaseInOut, animations: {
+            self.view.layoutIfNeeded()
+        })
+        
+        self.loginView.isHidden = false
+        self.registerView.isHidden = false
+        UIView.animate(withDuration: 0.4, delay: 0.4, options: .curveEaseInOut, animations: {
+            self.loginView.alpha = 1.0
+            self.registerView.alpha = 1.0
+        })
+    }
+    
+    @IBAction func didEndOnExitTextField(_ sender: Any) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func onTapLogin(_ sender: Any) {
+        
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func onTapRegister(_ sender: Any) {
+        
+        self.view.endEditing(true)
     }
 }
